ef6cd0a65736b80363d8b5a2ff71ebfaf212c84e
Ulises Gomez Jaramillo <ulisesgomezjaramillo@gmail.com>
2014-10-28 12:20:03 -0700

    added code to fix the error being recived earlier

--- controller/create-db.php
+++ controller/create-db.php
@@ -2,12 +2,17 @@
 	require_once(__DIR__ . "/../model/database.php");
 
 	$connection = new mysqli($host, $username, $password);
+	
 	if($connection->connect_error) {
 		die("Error: " . $connection->connect_error);
 	}
-	else {
-		echo "Success: " . $connection->host_info;
+	
+	$exists = $connection->select_db($database);
+
+	if(!$exists) {
+		echo "Database does not exist";
 	}
 
+	
 	$connection->close();
 ?>

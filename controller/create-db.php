14081475a0afb483fa95afd3651e5a341fe75a37
Ulises Gomez Jaramillo <ulisesgomezjaramillo@gmail.com>
2014-10-31 12:22:27 -0700

    no message

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,10 +1,12 @@
 <?php
+	//The require_once statement is identical to require except PHP will check if the file has already been included, 
+	//and if so, not include (require) it again.
 	require_once(__DIR__ . "/../model/database.php");
 
 	$connection = new mysqli($host, $username, $password);
 	
 	if($connection->connect_error) {
-		die("Error: " . $connection->connect_error);
+		die("Error: " . $connection->connect_error); //tells program to die off
 	}
 	
 	$exists = $connection->select_db($database);
@@ -20,18 +22,19 @@
 		echo "Database Has already been created";
 	}
 
-	$query = $connection->query("CREATE TABLE posts ("
-		. "id int (11) NOT NULL AUTO_INCREMENT, "
-		. "title varchar (255) NOT NULL,"
-		. "post text NOT NULL,"
-		. "PRIMARY KEY (id))");
+	$query = $connection->query("CREATE TABLE posts ("  //creates a table by query
+
+	. "id int(11) NOT NULL AUTO_INCREMENT,"				// creates unique ids for the server and cannot be empty and handles the ids for us 
+	. "title varchar(255) NOT NULL,"					// creates a collumn for a title with a max length of 255 characters and it cant be empty
+	. "post text NOT NULL,"								//creates a collumn for text that cannot be empty
+	. "PRIMARY KEY(id))");							// allows the tables to be connected and is shown that it is connected by an id
 	
-	if ($query) {
-		echo "Succesfully created table :posts";
+	if ($query) {									// checks to see if this statement exists
+		# code...
+		echo "<p>Successfully created table: posts</p>";   // lets you know that it succesfully created table:posts
 	}
-	else {
-		echo "$connection->error";
+	else{
+		echo "<p>$connection->error</p>";			//lets you know that table:posts already exists
 	}
-	
-	$connection->close();
+	$connection->close();							//closes connection 
 ?>

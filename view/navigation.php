df841de98b234df169893c3dae337e9158035abc
Ulises Gomez Jaramillo <ulisesgomezjaramillo@gmail.com>
2014-11-04 12:23:00 -0800

    inserted comments

--- view/navigation.php
+++ view/navigation.php
@@ -1,8 +1,13 @@
 <?php
+//goes to the config file
 	require_once(__DIR__ . "/../model/config.php");
 ?>
 <nav>
 	<ul>
-		<li><a href="<?php echo $path . "post.php"?>">Blog Post Form</a></li>
+		<li>
+		<!--Inserted the php into href to echo into a variable path and concatenates it with the string-->
+		<a href="<?php echo $path . "post.php"?>">Blog Post Form</a>
+		<!-- makes link to the post file -->
+		</li>
 	</ul>
 </nav>

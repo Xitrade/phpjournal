5013347c38cb8da1e4b3b1f8d023fcb426570038
Ulises Gomez Jaramillo <ulisesgomezjaramillo@gmail.com>
2014-11-05 12:22:33 -0800

    (finished videos 19, 20)

---
+++ post.php
@@ -0,0 +1,17 @@
+ <!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body id="ground">
+<?php
+
+require_once(__DIR__ . "/view/header.php");
+require_once(__DIR__ . "/view/form.php");
+require_once(__DIR__ . "/view/footer.php");
+
+?>
+
+</body>
+</html>

a4f4d4c3ce6119d34b31c5f744a6a8e30e78c8fe
Ulises Gomez Jaramillo <ulisesgomezjaramillo@gmail.com>
2014-11-06 12:04:08 -0800

    added media code to conform to iphone settings

--- view/header.php
+++ view/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <head>
 <meta charset="UTF-8">
 	<title></title>

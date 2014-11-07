c0b41cafe44873683b2372d721e180331723ada5
Ulises Gomez Jaramillo <ulisesgomezjaramillo@gmail.com>
2014-11-07 10:57:39 -0800

    no message

---
+++ controller/create-post.php
@@ -0,0 +1,8 @@
+<?php
+//the filter command gives a specific variable by name and filters it
+$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+
+echo "<p>Title: $title</p>";
+echo "<p>Post: $post</p>";
+?>

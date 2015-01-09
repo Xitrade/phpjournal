3a9f1766c67208e491946e3b5c62af0a00747e61
Ulises Gomez Jaramillo <ulisesgomezjaramillo@gmail.com>
2015-01-08 19:08:01 -0800

    final commits

--- view/footer.php
+++ view/footer.php
@@ -1,2 +1,5 @@
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src ="js/readmore.min.js"></script>
+	<script src ="js/read-more.js"></script>
 	</body>
 </html>
